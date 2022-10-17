cf6ccadd572168f99d5ab45ed29eeaf10fe2826f
Justin Parsons <parsonjj@bc.edu>
2022-10-17 14:24:41 -0400

    lost

---
+++ client.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"encoding/gob"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+
+type process_info struct {
+	ip   string
+	port string
+}
+
+func genRandShni(x int) []byte {
+	bas := make([]byte, x)
+	rand.Read(bas)
+	return bas
+}
+
+// parse_config takes the path pointing to a config file and translates it into a map indexed by process id containing process_info structs
+func parse_config(path string) (map[string]process_info) {
+
+	// initialize empty proccess map
+	processes := make(map[string]process_info)
+
+	// open config file and initialize a scanner
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	// read values into process map
+	for scanner.Scan() {
+		splitline := strings.Split(scanner.Text(), " ")
+		processes[splitline[0]] = process_info{splitline[1], splitline[2]}
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return processes
+}
+
+func initialize_outgoing(info process_info) net.Conn {
+	conn, err := net.Dial("tcp", info.ip+":"+info.port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return conn
+}
+
+func main() {
+
+	processes := parse_config("config")
+	//gob_connection := initialize_outgoing(processes["gob"])
+	json_connection := initialize_outgoing(processes["json"])
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		number, _ := strconv.Atoi(text[:len(text)-1])
+		randshni := genRandShni(number)
+		randshni2 := randshni
+		fmt.Println(randshni)
+/*
+		go func() {
+				enc := gob.NewEncoder(gob_connection)
+				fmt.Println(time.Now())
+				enc.Encode(randshni)	
+		} ()
+*/
+		go func() {
+				enc := gob.NewEncoder(json_connection)
+				fmt.Println(time.Now())
+				enc.Encode(randshni2)
+		} ()
+
+	}
+	
+}
---
+++ server.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/gob"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"time"
+)
+
+func initialize_source(s string) net.Listener {
+	if s == "gob" {
+		ln, err := net.Listen("tcp", "127.0.0.1:8082")
+		if err != nil {
+			log.Fatal(err)
+		}
+		return ln
+	}
+	if s == "json" {
+		ln, err := net.Listen("tcp", "127.0.0.1:8083")
+		if err != nil {
+			log.Fatal(err)
+		}
+		return ln
+	}
+	return nil
+}
+
+func gob_recieve(source net.Listener) {
+	for {
+
+		// accept incoming connections
+		conn, err := source.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// pass connection into subproccess to handle incoming messages
+		go func(conn net.Conn) {
+			dec := gob.NewDecoder(conn)
+			var shni []byte
+			for {
+				dec.Decode(&shni)
+				fmt.Println(time.Now())
+			}
+		}(conn)
+
+	}
+}
+
+func json_recieve(source net.Listener) {
+	for {
+		conn, err := source.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go func(conn net.Conn) {
+			dec := json.NewDecoder(conn)
+			var shni []byte
+			for {
+				dec.Decode(&shni)
+				fmt.Println(time.Now())
+			}
+		}(conn)
+	}
+}
+
+
+func main() {
+	args := os.Args
+	self := args[1]
+	server := initialize_source(self)
+	if self == "gob" {
+		gob_recieve(server)
+	}
+	if self == "json" {
+		json_recieve(server)
+	}
+	defer server.Close()
+}
